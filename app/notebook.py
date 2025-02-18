a81bf0bc4116117dadd5ad590412eaad7f5c6f82
Valentina <valentina.sierra.ospina.12@gmail.com>
2025-02-18 13:12:34 -0500

    se agrego la clase note

--- app/notebook.py
+++ app/notebook.py
@@ -1,1 +1,8 @@
 # TODO: Agrega el código de las clases del modelo aquí. Borra este comentario al terminar.
+
+
+class Note:
+        HIGH:str = "HIGH"
+        MEDIUM:str = "MEDIUM"
+        LOW:str = "LOW"
+
